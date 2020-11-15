06877de05010f659d1df93bbe6f3d6318ede23e2
Róbert Fikó <fiko.robert@gmail.com>
2020-11-15 21:16:00 +0100

    Test class added

---
+++ CrazyBot/CrazyBotTest/CrazyBotTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrazyBotTest
+{
+    [TestClass]
+    public class CrazyBotTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
